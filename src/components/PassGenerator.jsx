4e98fc21dc4ded4b6fc9cf4e472f80d599f832d1
kndshein <kndshein@gmail.com>
2024-06-14 21:21:41 -0700

    Add toast

--- src/components/PassGenerator.jsx
+++ src/components/PassGenerator.jsx
@@ -5,6 +5,7 @@ import generateGradient from '../utilities/generateGradient';
 import { HiOutlineClipboardCopy } from 'react-icons/hi';
 import { RiLoader5Fill } from 'react-icons/ri';
 import Password from './Password';
+import toast, { Toaster } from 'react-hot-toast';
 
 let colorChart = {
   isReversedOptimized: 'rgb(251, 113, 133)',
@@ -90,124 +91,140 @@ export default function PassGenerator() {
   }
 
   return (
-    <div className="flex flex-col max-w-2xl">
-      <p className="m-4 my-0 text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 to-sky-400 font-semibold">
-        Aardvark Secure Secret Password Generator
-      </p>
-      <section className="w-full rounded-lg px-4 top-0 sticky">
-        <div className="bg-zinc-900 pt-6">
-          <button
-            className="w-full rounded-lg mt-0 p-2 py-4 font-bold text-xl text-white transition ease-in-out hover:shadow-lg active:shadow-none active:scale-95 hover:shadow-gray-800"
-            onClick={() => handleOnSubmit()}
-            style={{
-              backgroundColor: submitGradient.color,
-              backgroundImage: submitGradient.gradient,
-            }}
-          >
-            Re-Generate Password
-          </button>
-          <div className="relative flex justify-center items-center h-16 w-100 mt-4 rounded-lg bg-gray-800">
-            {loading ? (
-              <RiLoader5Fill
-                className="animate-spin h-8 w-8"
-                style={{ color: 'grey' }}
-              />
-            ) : (
-              <Password text={generatedPassword} newRender={generationCount} />
-            )}
+    <>
+      <Toaster position="top-right" />
+      <div className="flex flex-col max-w-2xl">
+        <p className="m-4 my-0 text-3xl text-center bg-clip-text text-transparent bg-gradient-to-r from-cyan-300 to-sky-400 font-semibold">
+          Aardvark Secure Secret Password Generator
+        </p>
+        <section className="w-full rounded-lg px-4 top-0 sticky">
+          <div className="bg-zinc-900 pt-6">
             <button
-              onClick={() => navigator.clipboard.writeText(generatedPassword)}
-              className="absolute right-4 p-2 rounded-lg bg-gray-900 hover:bg-gray-700 active:bg-gray-950"
+              className="w-full rounded-lg mt-0 p-2 py-4 font-bold text-xl text-white transition ease-in-out hover:shadow-lg active:shadow-none active:scale-95 hover:shadow-gray-800"
+              onClick={() => handleOnSubmit()}
+              style={{
+                backgroundColor: submitGradient.color,
+                backgroundImage: submitGradient.gradient,
+              }}
             >
-              <HiOutlineClipboardCopy
-                className="h-6 w-6"
-                style={{ color: 'grey' }}
-              />
+              Re-Generate Password
             </button>
-          </div>
-          <p className="mt-2 text-left opacity-25 italic text-sm text-white">
-            * Remember your password by simply memorizing it.
-          </p>
-          {(opts.isDoubled || opts.isPowered) && (
-            <p className="text-left opacity-25 italic text-sm text-white">
-              ** We&apos;ve been told that our developers don&apos;t know how to
-              code a shopping cart, so I guess it&apos;s on the house.
+            <div className="relative flex justify-center items-center h-16 w-100 mt-4 rounded-lg bg-gray-800">
+              {loading ? (
+                <RiLoader5Fill
+                  className="animate-spin h-8 w-8"
+                  style={{ color: 'grey' }}
+                />
+              ) : (
+                <Password
+                  text={generatedPassword}
+                  newRender={generationCount}
+                />
+              )}
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(generatedPassword);
+                  toast.success('Password copied!', {
+                    style: {
+                      borderRadius: '8px',
+                      border: '1px solid #22d3ee',
+                      background: '#1f2937',
+                      color: '#d1d5db',
+                    },
+                  });
+                }}
+                className="absolute right-4 p-2 rounded-lg bg-gray-900 hover:bg-gray-700 active:bg-gray-950"
+              >
+                <HiOutlineClipboardCopy
+                  className="h-6 w-6"
+                  style={{ color: 'grey' }}
+                />
+              </button>
+            </div>
+            <p className="mt-2 text-left opacity-25 italic text-sm text-white">
+              * Remember your password by simply memorizing it.
             </p>
-          )}
+            {(opts.isDoubled || opts.isPowered) && (
+              <p className="text-left opacity-25 italic text-sm text-white">
+                ** We&apos;ve been told that our developers don&apos;t know how
+                to code a shopping cart, so I guess it&apos;s on the house.
+              </p>
+            )}
+          </div>
+          <div className="h-10 bg-gradient-to-b from-zinc-900 from-15%"></div>
+        </section>
+        <div className="-mt-4 border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
+          <Checkbox
+            value={opts.isBothOptimized || !opts.isDictOptimized}
+            name="isReverseOptimized"
+            desc="Optimize for Reverse Dictionary Attacks"
+            long_desc="By optimizing your password for reverse dictionary attacks, you allow other users to be hacked first. Outrunning the slowest of the group and whatnot."
+            handleOnClick={handleOnClick}
+            disabled={opts.isBothOptimized}
+            is_round
+          />
+          <Checkbox
+            value={opts.isDictOptimized}
+            name="isDictOptimized"
+            desc="Optimize for Regular Dictionary Attacks"
+            long_desc="Sometimes it is better to simply brace for what's common. We can barely understand regular dictionary attacks, why bother with the reverse?"
+            handleOnClick={handleOnClick}
+            disabled={opts.isBothOptimized}
+            is_round
+          />
+          <hr className="border-gray-500 m-2" />
+          <Checkbox
+            value={opts.isBothOptimized}
+            name="isBothOptimized"
+            desc="Optimize for both types of Dictionary Attacks"
+            long_desc="Things are best when used in moderation -- or so we tell ourselves. This is for those of us out there who enjoy the idea of commitment without actually committing."
+            handleOnClick={handleOnClick}
+          />
         </div>
-        <div className="h-10 bg-gradient-to-b from-zinc-900 from-15%"></div>
-      </section>
-      <div className="-mt-4 border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
-        <Checkbox
-          value={opts.isBothOptimized || !opts.isDictOptimized}
-          name="isReverseOptimized"
-          desc="Optimize for Reverse Dictionary Attacks"
-          long_desc="By optimizing your password for reverse dictionary attacks, you allow other users to be hacked first. Outrunning the slowest of the group and whatnot."
-          handleOnClick={handleOnClick}
-          disabled={opts.isBothOptimized}
-          is_round
-        />
-        <Checkbox
-          value={opts.isDictOptimized}
-          name="isDictOptimized"
-          desc="Optimize for Regular Dictionary Attacks"
-          long_desc="Sometimes it is better to simply brace for what's common. We can barely understand regular dictionary attacks, why bother with the reverse?"
-          handleOnClick={handleOnClick}
-          disabled={opts.isBothOptimized}
-          is_round
-        />
-        <hr className="border-gray-500 m-2" />
-        <Checkbox
-          value={opts.isBothOptimized}
-          name="isBothOptimized"
-          desc="Optimize for both types of Dictionary Attacks"
-          long_desc="Things are best when used in moderation -- or so we tell ourselves. This is for those of us out there who enjoy the idea of commitment without actually committing."
-          handleOnClick={handleOnClick}
-        />
+        <div className="border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
+          <Checkbox
+            value={opts.isUppercased}
+            name="isUppercased"
+            desc="Include Uppercase Letters"
+            long_desc="Uppercase some letters for better security, but because it's still the same character, they're easier to remember."
+            example="AARDVARKSARENOTPIGS"
+            handleOnClick={handleOnClick}
+          />
+        </div>
+        <div className="border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
+          <Checkbox
+            className="border-emerald-400"
+            value={opts.isLeeted}
+            name="isLeeted"
+            desc="Include Leeted Letters"
+            long_desc="Leet speak your password so AI robots can't read it. Post your leeted password on Tumblr for massive street creds."
+            example="i, l, 1, L, o, 0, O"
+            handleOnClick={handleOnClick}
+          />
+        </div>
+        <div className="p-2 mx-4 mb-6 rounded-lg rotating-border">
+          <Checkbox
+            className="border-orange-400"
+            value={opts.isDoubled}
+            name="isDoubled"
+            desc="Double the Security"
+            price="$7.99"
+            long_desc="For just $7.99 biweekly subscription renewed every month, double the 'hashing rate' of your password. Just like the Double Quarter Pounder, two Deadpool films, and at least two front teeth, things are better when they're doubled."
+            handleOnClick={handleOnClick}
+          />
+        </div>
+        <div className="p-2 mx-4 mb-6 rounded-lg rotating-border">
+          <Checkbox
+            className="border-orange-400"
+            value={opts.isPowered}
+            name="isPowered"
+            desc="Double the Processing Power"
+            price="¥200"
+            long_desc="Research shows that loading icons that spin for just the right amount of time make users think the server worked hard, r('murica) = .42, p = .69. (Saysnoone at el., 420 BC) Anyway, this feature definitely doesn't just render a fake loading icon, why do you ask?"
+            handleOnClick={handleOnClick}
+          />
+        </div>
       </div>
-      <div className="border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
-        <Checkbox
-          value={opts.isUppercased}
-          name="isUppercased"
-          desc="Include Uppercase Letters"
-          long_desc="Uppercase some letters for better security, but because it's still the same character, they're easier to remember."
-          example="AARDVARKSARENOTPIGS"
-          handleOnClick={handleOnClick}
-        />
-      </div>
-      <div className="border-2 border-cyan-400 p-2 mx-4 mb-6 rounded-lg">
-        <Checkbox
-          className="border-emerald-400"
-          value={opts.isLeeted}
-          name="isLeeted"
-          desc="Include Leeted Letters"
-          long_desc="Leet speak your password so AI robots can't read it. Post your leeted password on Tumblr for massive street creds."
-          example="i, l, 1, L, o, 0, O"
-          handleOnClick={handleOnClick}
-        />
-      </div>
-      <div className="p-2 mx-4 mb-6 rounded-lg rotating-border">
-        <Checkbox
-          className="border-orange-400"
-          value={opts.isDoubled}
-          name="isDoubled"
-          desc="Double the Security"
-          price="$7.99"
-          long_desc="For just $7.99 biweekly subscription renewed every month, double the 'hashing rate' of your password. Just like the Double Quarter Pounder, two Deadpool films, and at least two front teeth, things are better when they're doubled."
-          handleOnClick={handleOnClick}
-        />
-      </div>
-      <div className="p-2 mx-4 mb-6 rounded-lg rotating-border">
-        <Checkbox
-          className="border-orange-400"
-          value={opts.isPowered}
-          name="isPowered"
-          desc="Double the Processing Power"
-          price="¥200"
-          long_desc="Research shows that loading icons that spin for just the right amount of time make users think the server worked hard, r('murica) = .42, p = .69. (Saysnoone at el., 420 BC) Anyway, this feature definitely doesn't just render a fake loading icon, why do you ask?"
-          handleOnClick={handleOnClick}
-        />
-      </div>
-    </div>
+    </>
   );
 }
